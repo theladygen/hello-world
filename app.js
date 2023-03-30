c2f49a21139a1ba44b4e17719747d60db6379fc9
Genevieve DuPuy <genevieve.e.dupuy@gmail.com>
2023-03-30 00:59:24 -0700

    autoformatted with prettier

--- app.js
+++ app.js
@@ -1,27 +1,24 @@
-let userName = prompt ("What's your name?");
-console.log ("userName is", userName);
+let userName = prompt("What's your name?");
+console.log("userName is", userName);
 alert("Hi " + userName + ", welcome to Studio Le Mew! 😸");
 
 function pickACat() {
-    let favoriteCat = prompt ("Now that you've met all of us fine felines, do tell: who's your favorite cat??");
-    favoriteCat = favoriteCat.toLowerCase();
-    console.log ("favorite cat", favoriteCat);
-    if (favoriteCat == "oliver") {
-        alert("Bold choice. I like it.");
-    }
-    else if (favoriteCat == "sophie") {
-        alert("She's the peachiest of peaches, it's true!");
-    }
-    else if (favoriteCat == "fox") {
-        alert("Foxy is the bestest boy!");
-    }
-    else if (favoriteCat == "albus") {
-        alert("The most handsome, distinguished gentleman in the bunch!");
-    }
-    else if (favoriteCat == "bug") {
-        alert("Aw, who doesn't love a love Bug?!");
-    }
-    else {
-        alert("Ahhhh interesting. You should introduce us!");
-    }
+  let favoriteCat = prompt(
+    "Now that you've met all of us fine felines, do tell: who's your favorite cat??"
+  );
+  favoriteCat = favoriteCat.toLowerCase();
+  console.log("favorite cat", favoriteCat);
+  if (favoriteCat == "oliver") {
+    alert("Bold choice. I like it.");
+  } else if (favoriteCat == "sophie") {
+    alert("She's the peachiest of peaches, it's true!");
+  } else if (favoriteCat == "fox") {
+    alert("Foxy is the bestest boy!");
+  } else if (favoriteCat == "albus") {
+    alert("The most handsome, distinguished gentleman in the bunch!");
+  } else if (favoriteCat == "bug") {
+    alert("Aw, who doesn't love a love Bug?!");
+  } else {
+    alert("Ahhhh interesting. You should introduce us!");
+  }
 }
